c04bed3e6e5e16e49dd15060590a08dd3d5a78dd
Komal Sinha <komalsinha1811@gmail.com>
2025-10-07 15:32:08 +0530

    add avatar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
 
     return (
         <div className="flex min-h-screen home-wrapper">
-            Home
+
             <section className="grid w-full gap-8 home-section">
                 <div className="md:col-span-1 xl:col-span-1">
                     <TradingViewWidget
